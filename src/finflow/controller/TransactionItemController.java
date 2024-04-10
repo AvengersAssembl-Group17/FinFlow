c7f437b2ea85c40c3d056381d01d319d29d4d430
Hazel Lobo <lobo.ha@northeastern.edu>
2024-04-10 18:38:35 -0400

    Updated transaction specific img to trans listing

--- src/finflow/controller/TransactionItemController.java
+++ src/finflow/controller/TransactionItemController.java
@@ -1,13 +1,18 @@
 package finflow.controller;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import finflow.dao.DatabaseConnection;
+import finflow.dao.TransactionDAO;
+import finflow.dao.TransactionDAOImpl;
 import finflow.model.Transaction;
 import finflow.utils.Constants;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
@@ -28,8 +33,26 @@ public class TransactionItemController implements Initializable{
     @FXML
     private Label transactionTitle;
     
+    private TransactionDAO transDAO;
+    
+    public TransactionItemController() {
+    	this.transDAO = new TransactionDAOImpl(new DatabaseConnection());
+    }
+    
     public void setData(Transaction trans) {
     	String formattedAmount = String.format(Constants.CURRENCY_FORMAT, trans.getAmount());
+    	String transactionType = transDAO.getTransactionTypeNameById(trans.getType());
+    	System.out.println(transactionType);
+        String imagePath = "/finflow/images/" + Constants.IMAGE_MAP.get(transactionType) + ".png";
+
+        System.out.println(transactionType);
+        InputStream imageStream = getClass().getResourceAsStream(imagePath);
+        if (imageStream != null) {
+            Image transImg = new Image(imageStream);
+            transactionImg.setImage(transImg);
+        }else {
+        	System.out.println("No Image found for "+transactionType + ", setting default image");
+        }
     	transactionTitle.setText(trans.getTitle());
     	transactionDate.setText(trans.getDate().toString());
     	transactionNotes.setText(trans.getNotes());
