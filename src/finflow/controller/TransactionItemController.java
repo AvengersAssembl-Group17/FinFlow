63f2da3eebc90ed3888185aaad68e4a84b8c1cf5
SaurabhSoniNEU <144939319+SaurabhSoniNEU@users.noreply.github.com>
2024-04-11 23:31:59 -0400

    Added Update & Delete modified TransactionItemController

--- src/finflow/controller/TransactionItemController.java
+++ src/finflow/controller/TransactionItemController.java
@@ -1,17 +1,22 @@
 package finflow.controller;
 
+import finflow.dao.DatabaseConnection;
+import finflow.dao.TransactionDAO;
+import finflow.dao.TransactionDAOImpl;
+import finflow.model.Transaction;
+import finflow.utils.Constants;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import finflow.model.Transaction;
-import finflow.utils.Constants;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-
-
-public class TransactionItemController implements Initializable{
+public class TransactionItemController implements Initializable {
 
     @FXML
     private Label transactionAmount;
@@ -20,26 +25,95 @@ public class TransactionItemController implements Initializable{
     private Label transactionDate;
 
     @FXML
-    private ImageView transactionImg;
-
-    @FXML
     private Label transactionNotes;
 
     @FXML
     private Label transactionTitle;
-    
+
+    @FXML
+    private TextField newAmountField;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Button deleteButton;
+
+    private Transaction transaction;
+
+    private TransactionHistoryController historyController;
+
     public void setData(Transaction trans) {
-    	String formattedAmount = String.format(Constants.CURRENCY_FORMAT, trans.getAmount());
-    	transactionTitle.setText(trans.getTitle());
-    	transactionDate.setText(trans.getDate().toString());
-    	transactionNotes.setText(trans.getNotes());
-    	transactionAmount.setText(formattedAmount);
+        this.transaction = trans;
+        String formattedAmount = String.format(Constants.CURRENCY_FORMAT, trans.getAmount());
+        transactionTitle.setText(trans.getTitle());
+        transactionDate.setText(trans.getDate().toString());
+        transactionNotes.setText(trans.getNotes());
+        transactionAmount.setText(formattedAmount);
     }
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void setHistoryController(TransactionHistoryController historyController) {
+        this.historyController = historyController;
+    }
 
+    @FXML
+    private void handleUpdateTransaction(ActionEvent event) {
+        if (transaction == null) {
+            System.err.println("Transaction is null.");
+            return;
+        }
+
+        // Make the text field visible when clicking the update button
+        newAmountField.setVisible(true);
+
+        String newAmountText = newAmountField.getText().trim();
+
+        double newAmount;
+        try {
+            newAmount = Double.parseDouble(newAmountText);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid new amount.");
+            return;
+        }
+
+        TransactionDAO transactionDAO = new TransactionDAOImpl(new DatabaseConnection());
+        transaction.setAmount(newAmount);
+        int rowsAffected = transactionDAO.updateTransactionAmount(transaction.getTransactionId(), newAmount);
+        if (rowsAffected > 0) {
+            String formattedAmount = String.format(Constants.CURRENCY_FORMAT, newAmount);
+            transactionAmount.setText(formattedAmount);
+        } else {
+            System.err.println("Failed to update transaction amount.");
+        }
+    }
+
+    @FXML
+    private void handleDeleteTransaction(ActionEvent event) {
+        if (transaction == null) {
+            System.err.println("Transaction or historyController is null.");
+            return;
+        }
+
+        TransactionDAO transactionDAO = new TransactionDAOImpl(new DatabaseConnection());
+        int rowsAffected = transactionDAO.deleteTransaction(transaction.getTransactionId());
+        if (rowsAffected > 0) {
+            System.out.println("Transaction deleted successfully.");
+            
+            //historyController.populateTransaction();
+            // Repopulate the transaction history in the UI
+        } else {
+            System.err.println("Failed to delete transaction.");
+        }
+    }
+
+
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialization logic
+    }
 }
